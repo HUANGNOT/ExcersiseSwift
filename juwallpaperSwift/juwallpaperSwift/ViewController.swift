79575147a47372a3aad4d367956387a749ebf055
tongxia <tongxia_2010@126.com>
2016-07-05 14:42:17 +0800

    Swift Exercise
    Learning Swift Programmign

---
+++ juwallpaperSwift/juwallpaperSwift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  juwallpaperSwift
+//
+//  Created by duoyi on 16/7/5.
+//  Copyright © 2016年 Personal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
